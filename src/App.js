95b399eaa6a93cc953173d9bab83b1c833cd8b4f
Jefferson Souza <jefferson.ti@hotmail.com>
2024-10-25 16:59:12 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cadastro" element={<Cadastro />} />
